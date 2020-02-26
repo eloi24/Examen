95e2f638a785f036d9da8bab7bf7d084b1f8ca61
eloi24 <eloibaucid24@gmail.com>
2020-02-26 19:01:33 +0100

    Hola

--- Prova/src/Blog.java
+++ Prova/src/Blog.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import clases.*;
+
 public class Blog{
     public static void main(String[] args) {
-        
+     ArrayList<Usuari> usuaris = new ArrayList<>();
+     ArrayList<Post> posts = new ArrayList<>();
+     Scanner sc = new Scanner(System.in);
+     System.out.println("*********************************");
+     System.out.println("* Benvingut a CirvSocialNetwork *");
+     System.out.println("*********************************");
+     System.out.println("Introdueix el nom de l'usuari que tindrà el rol d'administrador: ");
+     String nomusuari=sc.nextLine();
+     usuaris.add(new Admin(nomusuari, passwd))
     }
 }
---
+++ Prova/src/functions/Funcions.java
@@ -0,0 +1,12 @@
+package functions;
+
+import java.util.Scanner;
+
+public class Funcions{
+    public static Scanner sc = new Scanner(System.in);
+    public static boolean comprovapasswd(String passwd){
+      System.out.println("");
+
+
+    }
+}
